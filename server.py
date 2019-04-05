632cfdecc539c3587ef271232cc2806dc953a91f
SoBeRBot94 <sudarsan.bhargavan94@gmail.com>
2019-04-05 21:41:24 +0200

    Added Package Data & and Vendor APIs

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import datetime
 from functools import wraps
+import requests
 
 
 app = Flask(__name__)
@@ -27,20 +28,21 @@ class Vendor(db.Model):
     name = db.Column(db.String(25))
     password = db.Column(db.String(80))
 
-class Inventory(db.Model):
+class Package(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     qr_id = db.Column(db.String(50), unique=True)
     name = db.Column(db.String(25))
     volume = db.Column(db.Integer)
     vendorName = db.Column(db.String(25))
     inception = db.Column(db.String(25))
-    intermediate = db.Column(db.Boolean)
-    interDestWarehouse = db.Column(db.String(25))
+    intraCityDelivery = db.Column(db.Boolean)
+    deliveryLocation = db.Column(db.String(50))
     pickupTime = db.Column(db.DateTime(timezone=False))
-    mappedEmployeeID = db.Column(db.String(25), unique=True)
     packageStatus = db.Column(db.String(10))
     dropTime = db.Column(db.DateTime(timezone=False))
     packageCurrentLocation = db.Column(db.String(25))
+
+# Employee
 
 def employeeToken(f):
     @wraps(f)
@@ -150,7 +152,7 @@ def delete_employee(currentEmployee, public_id):
     return jsonify({'Message': 'Employee \t' + employee.name + '\t has been Removed!'})
 
 @app.route('/employeelogin', methods=['GET'])
-def login():
+def employeeLogin():
     auth = request.authorization
 
     if not auth or not auth.username or not auth.password:
@@ -168,6 +170,114 @@ def login():
     
     return make_response('Could not Verify', 401, {'WWW-Authenticate' : 'Basic realm="Login Required!"'})
 
+# Vendor
+
+def vendorToken(f):
+    @wraps(f)
+    def decoratedFunction(*args, **kargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'Message' : 'Token is Missing!'}), 401
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            currentVendor = Vendor.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'Message' : 'Token is Invalid!'}), 401
+
+        return f(currentVendor, *args, **kargs)
+
+    return decoratedFunction
+
+@app.route('/vendorlogin', methods=['GET'])
+def vendorLogin():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not Verify', 401, {'WWW-Authenticate' : 'Basic realm="Login Required!"'})
+
+    vendor = Vendor.query.filter_by(name=auth.username).first()
+
+    if not vendor:
+        return make_response('Could not Verify', 401, {'WWW-Authenticate' : 'Basic realm="Login Required!"'})
+
+    if check_password_hash(vendor.password, auth.password):
+        token = jwt.encode({'public_id' : vendor.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=1440)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+    
+    return make_response('Could not Verify', 401, {'WWW-Authenticate' : 'Basic realm="Login Required!"'})
+
+@app.route('/vendor', methods=['POST'])
+@employeeToken
+def create_vendor(currentEmployee):
+    
+    if not currentEmployee.admin:
+        return jsonify({'Message' : 'Cannot Perform Action, You Are Not an Admin!'})
+   
+    vendorData = request.get_json()
+
+    hashedPassword = generate_password_hash(vendorData['password'], method='sha256')
+
+    newVendor = Vendor(public_id=str(uuid.uuid4()), name=vendorData['name'], password=hashedPassword)
+
+    db.session.add(newVendor)
+    db.session.commit()
+
+    return jsonify({'Message' : 'New Vendor has been created!'})
+
+# Data API
+
+@app.route('/fetchdata', methods=['POST'])
+@vendorToken
+def fetch_data(currentVendor):
+
+    geoLocateURL = 'https://api.ipgeolocation.io/ipgeo'
+    APIKey = 'f38b38d7de524ab2b337dba786fabec0'
+    geoLocateData = requests.get(geoLocateURL+'?apiKey='+APIKey+'&fields=city')
+
+    packageData = request.get_json()
+    
+    newPackage = Package(qr_id=str(uuid.uuid4()), name=packageData['name'],
+
+                         volume=packageData['volume'],
+
+                         vendorName=currentVendor.name,
+
+                         inception=packageData['inception'],
+
+                         intraCityDelivery=packageData['intraCityDelivery'],
+
+                         deliveryLocation=packageData['deliveryLocation'],
+
+                         packageStatus='WAITING',
+
+                         packageCurrentLocation=geoLocateData)
+
+    db.session.add(newPackage)
+    db.session.commit()
+
+    return jsonify({'Message' : 'The Package has been added!'})
+
+@app.route('/fetchdata/<qr_id>', methods=['DELETE'])
+@vendorToken
+def delete_data(currentVendor, qr_id):
+
+    package = Package.query.filter_by(qr_id=qr_id).first()
+
+    if not package:
+        return jsonify({'Message': 'No Package Found to Remove!'})
+
+    db.session.delete(package)
+    db.session.commit()
+
+    return jsonify({'Message': 'Package \t' + package.name + '\t has been Removed!'})
+
+    
 
 if __name__ == '__main__':
     app.run()
